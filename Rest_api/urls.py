882c371f6178c4bc1600917dc98d34054b8d4d5a
Bernadette Chukwuedo <chukwuedoogochukwubernadette@gmail.com>
2022-11-05 00:32:01 +0100

    Arithmetic operation

--- Rest_api/urls.py
+++ Rest_api/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
 
     path('', include('arithmetic_operation.urls'))
-]
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
